c66d7b5511e16aa44331a0f767c5c9785791152e
craftopc <lhon901@163.com>
2024-07-26 10:53:19 +0800

    Add moudle -> "neovim"; Feat neovim -> "options.lua"

---
+++ neovim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,17 @@
+local opt = vim.opt
+
+opt.number = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.autoindent = true
+opt.expandtab = true
+opt.list = true
+opt.cursorline = true
+opt.cmdheight = 2
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true -- default: on
+opt.incsearch = true -- default: on
+opt.wildmenu = true -- default: on
+
