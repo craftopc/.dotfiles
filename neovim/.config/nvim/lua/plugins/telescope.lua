f901a16925969478214ff249fc4b414a7843bb82
craftopc <lhon901@163.com>
2024-07-27 16:58:28 +0800

    Perf neovim -> "Telescope"

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,6 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    lazy = true,
+    cmd = "Telescope",
+}
