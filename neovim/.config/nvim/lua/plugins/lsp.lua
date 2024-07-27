b75276435b998b598580ab900afd072e3bf01e57
craftopc <lhon901@163.com>
2024-07-27 17:59:45 +0800

    Feat neovim -> "lsp"

---
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,36 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        },
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "mason.nvim",
+        },
+        opts = {
+            ensure_installed = { "lua_ls"},
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "mason-lspconfig.nvim"
+        },
+        config = function()
+            require('lspconfig').lua_ls.setup {
+                settings = {
+                    ['lua_ls'] = {},
+                },
+            }
+        end,
+    },
+}
