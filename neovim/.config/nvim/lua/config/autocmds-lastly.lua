00b750cd8d3b23736632b894e051d8e5a9c23ac4
craftopc <lhon901@163.com>
2024-07-27 16:03:26 +0800

    Fix neovim -> "treesitter auto install"

--- neovim/.config/nvim/lua/config/autocmds-lastly.lua
+++ neovim/.config/nvim/lua/config/autocmds-lastly.lua
@@ -1,1 +1,4 @@
 vim.cmd('colorscheme catppuccin')
+
+-- auto update for treesitter
+vim.cmd('TSUpdate lua c python java markdown markdown_inline vimdoc query diff bash fish html javascript json regex toml yaml ini')
