5db148bc80f170d005d6627fc2c417360517609c
craftopc <lhon901@163.com>
2024-08-05 13:13:42 +0800

    Feat and Perf neovim -> "colorscheme"
    add tokyonight and perf catppuccin

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,24 @@ return {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
-        opts = {transparent_background = true}
-    }
+        lazy = true,
+        opts = {
+            transparent_background = true,
+            dashboard = true,
+            flash = true,
+            mason = false,
+            cmp = true,
+            dap = true,
+            dap_ui = true,
+            notify = false,
+            nvimtree = true,
+            treesitter = true,
+            rainbow_delimiter = true,
+            telescope = {
+                enabled = true
+            },
+            lsp_trouble = false,
+            which_key = false
+        }
+    }, {"folke/tokyonight.nvim", lazy = true, priority = 1000, opts = {}}
 }
