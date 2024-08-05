21b9b715616e2a409d38b949733b674004be9d82
craftopc <lhon901@163.com>
2024-08-05 17:29:42 +0800

    Feat neovim -> Add "mini"

---
+++ neovim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,13 @@
+return {
+    "echasnovski/mini.nvim",
+    version = false,
+
+    config = function ()
+        require("mini.pairs").setup()
+        require("mini.diff").setup()
+        require("mini.files").setup()
+        require("mini.cursorword").setup()
+        require("mini.icons").setup()
+        -- require("mini.surround").setup()
+    end
+}
