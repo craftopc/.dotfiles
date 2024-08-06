38a471e81bcc111c047d757dec28bec1e6b1a24c
craftopc <lhon901@163.com>
2024-08-06 10:45:53 +0800

    Feat neovim -> Add "dap"

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -34,11 +34,20 @@ map('v', 'K', ":m '<-2<CR>gv=gv")
 -----------------
 
 -- tree
-map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
+map('n', '<leader>e', ':Neotree toggle<CR>', opts)
 
 -- Telescope
 map('n', '<leader><leader>', ':Telescope fd theme=dropdown<CR>', opts)
 map('n', '<leader>b', ':Telescope buffers theme=dropdown<CR>', opts)
+map('n', '<leader>uc', ':Telescope colorscheme<CR>', opts)
+map('n', '<leader>cc', ':lua require("telescope.builtin").find_files({cwd="~/.dotfiles/neovim/.config/nvim"})<CR>', opts)
 
 -- conform (format)
 map('n', '<leader>cf', ':Format<CR>', opts)
+
+-- dap
+map("n", '<leader>ds', ':DapNew<CR>', opts)
+map("n", '<leader>db', ':DapToggleBreakpoint<CR>', opts)
+map("n", '<leader>di', ':DapStepInto<CR>', opts)
+map('n', '<leader>do', ':DapStepOver<CR>', opts)
+map('n', '<leader>dt', ':DapStepOut<CR>', opts)
---
+++ neovim/.config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,44 @@
+return {
+    {
+        "mfussenegger/nvim-dap",
+        config = function()
+            local dap = require('dap')
+
+            dap.adapters.cppdbg = {
+                id = 'cppdbg',
+                type = 'executable',
+                command = '/home/craftopc/.local/share/nvim/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7'
+            }
+
+            dap.configurations.c = {
+                {
+                    name = "Launch file",
+                    type = "cppdbg",
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input('Path to executable: ',
+                                            vim.fn.getcwd() .. '/', 'file')
+                    end,
+                    cwd = '${workspaceFolder}',
+                    stopAtEntry = true
+                }
+            }
+
+            dap.configurations.cpp = dap.configurations.c
+        end
+    }, {
+        "rcarriga/nvim-dap-ui",
+        version = false,
+        dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+        opts = {controls = {element = 'repl', enabled = 'true'}}
+    }, {
+        "jay-babu/mason-nvim-dap.nvim",
+        dependencies = {"williamboman/mason.nvim", "mfussenegger/nvim-dap"}
+    },
+    {
+        'theHamsta/nvim-dap-virtual-text',
+        dependencies = {"mfussenegger/nvim-dap", "nvim-treesitter/nvim-treesitter"},
+        opts = {}
+    }
+
+}
