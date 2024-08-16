03f0efb1a31be4e1012c17d3742c72b0f1b13e3e
craftopc <lhon901@163.com>
2024-08-16 23:44:04 +0800

    Perf telescope

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,33 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = {'nvim-lua/plenary.nvim'},
-    lazy = true,
-    cmd = "Telescope"
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        cmd = "Telescope",
+        opts = {
+            extensions = {
+                fzf = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = "smart_case"
+                },
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown({}),
+                },
+            },
+        },
+        config = function()
+            require("telescope").load_extension("ui-select")
+            require("telescope").load_extension("fzf")
+        end,
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        ft = "mason",
+    },
+    {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+    },
 }
