d0f55eb4b2d7d6167a8be783f43e1e5c47186f4b
craftopc <lhon901@163.com>
2024-08-23 20:18:01 +0800

    add lsp rust

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -6,45 +6,52 @@ return {
                 icons = {
                     package_installed = "✓",
                     package_pending = "➜",
-                    package_uninstalled = "✗"
-                }
-            }
-        }
-    }, {
+                    package_uninstalled = "✗",
+                },
+            },
+        },
+    },
+    {
         "williamboman/mason-lspconfig.nvim",
-        dependencies = {"mason.nvim"},
-        opts = {ensure_installed = {"lua_ls"}}
-    }, {
+        dependencies = { "mason.nvim" },
+        opts = { ensure_installed = { "lua_ls" } },
+    },
+    {
         "neovim/nvim-lspconfig",
-        dependencies = {"mason-lspconfig.nvim"},
+        dependencies = { "mason-lspconfig.nvim" },
 
         config = function()
             local lsp_configurations = {
                 lua_ls = {
                     settings = {
-                        ['lua_ls'] = {}
-                    }
+                        ["lua_ls"] = {},
+                    },
                 },
                 clangd = {
                     settings = {
-                        ['clangd'] = {}
-                    }
+                        ["clangd"] = {},
+                    },
                 },
                 rust_analyzer = {
                     settings = {
-                        ['rust_analyzer'] = {}
-                    }
+                        ["rust_analyzer"] = {},
+                    },
                 },
                 pyright = {
                     settings = {
-                        ['pyright'] = {}
-                    }
+                        ["pyright"] = {},
+                    },
+                },
+                ruff_lsp = {
+                    settings = {
+                        ["ruff_lsp"] = {},
+                    },
                 },
             }
 
             for lsp_name, lsp_settings in pairs(lsp_configurations) do
-                require('lspconfig')[lsp_name].setup(lsp_settings)
+                require("lspconfig")[lsp_name].setup(lsp_settings)
             end
-        end
-    }
+        end,
+    },
 }
