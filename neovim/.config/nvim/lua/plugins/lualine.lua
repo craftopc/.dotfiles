d903e1b5fcd5215aa403303b71ae679c230962ac
craftopc <lhon901@163.com>
2024-08-23 20:15:49 +0800

    Perf neovim -> lualine

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -1,18 +1,28 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = {'nvim-tree/nvim-web-devicons'},
-    opts = {
-        options = {
-            theme = "catppuccin",
-            disabled_filetypes = {
-                statusline = {'dashboard', 'lazy', 'neo-tree'},
-                winbar = {}
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = function()
+        local opts = {
+            options = {
+                theme = "catppuccin",
+                globalstatus = true,
+                disabled_filetypes = {
+                    statusline = { "dashboard" , "TelescopePrompt"},
+                    winbar = {},
+                },
+                ignore_focus = {
+                    "dap-repl",
+                    "dapui_watches",
+                    "dapui_stacks",
+                    "dapui_breakpoints",
+                    "dapui_scopes",
+                    "dapui_console",
+                },
             },
-            ignore_focus = {
-                "dap-repl", "dapui_watches", "dapui_stacks",
-                "dapui_breakpoints", "dapui_scopes", "dapui_console"
-            },
-            extensions = {"nvim-dap-ui"}
+            extensions = { "neo-tree", "lazy", "mason"},
         }
-    }
+
+        return opts
+    end,
 }
